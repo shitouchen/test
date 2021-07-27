1a7d6bcceb423ba7fb14b0b5862c291a77c49749
shitouchen <cl18810078360@163.com>
2021-07-27 22:40:37 +0800

    Save before ejecting

--- app/my-app/README.md
+++ app/my-app/README.md
@@ -1,6 +1,64 @@
 # 创建一个typescript格式的react-app
 npx create-react-app my-app --template typescript
 
+# 项目文件下npm run eject
+npm run eject会复制所有依赖文件和相应的依赖（webpack、babel等）到你的项目。是个单向的操作，一旦eject,npm run eject的操作是不可逆的
+
+# npm install --save typescript @types/node @types/react @types/react-dom @types/jest
+types的依赖
+
+# 在tsx文件中，不会识别.css文件，需要设置*.d.ts文件
+## 如： xxx.d.ts
+### 
+```javascript
+declare module "*.css"{
+    const css : {[key:string]:string}
+    export default css;
+}
+```
+# 图片文件的配置一般放在assets文件夹下，文件夹内部有fonts、icons、images
+## 若使用特殊字体，需在网上自行下载
+
+# State和Props的区别
+### props是组件对外的接口，而state是组件对内的接口
+### props用于组件间数据传递，而state用于组件内部的数据传递
+### state的状态改变只能使用this.setState，而constructor里是唯一存放setState的；
+### setState是异步的，不会立即处理state
+### 本质上，props就是传入函数的参数，是从传入组件内部的数据。更准确的说，是从父组件传递向子组件的数据。
+
+# Immutable：不变的
+### 对象一旦创建就不可改变，只能通过销毁、重建来改变数据
+### 通过判断内存地址的是否一致，来确认对象是否有经过修改。
+
+# 处理异步、动态显示
+### AJAX
+### callback 回调函数
+### 程序栈stack，先进先出
+
+# any 的理解
+### 资源来源于网络请求，返回的数据类型不受控制
+### 前端强行定义API数据类型，违反前后端分离的原则
+### 不能为了使用Type而放弃javascript的灵活性
+
+# React组件的生命周期
+### Mounting: 创建虚拟DOM，渲染UI
+### Updating: 更新虚拟DOM，重新渲染UI
+### Unmounting: 删除虚拟DOM，移除UI
+### 初始化：构建函数、getDerivedStateFormProps-->render():渲染UI-->componentDidMount
+### getDeriveStateFormProps-->shouldComponentUpdate-->render: 渲染UI-->更新-->componentDidUpdate
+### componentWillUnmount-->销毁
+
+# 高阶组件
+### HOC的公式：const hoc = higherOrder(wrappedComponent);
+### 高阶组件(HOC)就是一个返回了组件的函数；
+### 通过组件嵌套的方法给子组件添加更多的功能；
+### 接收一个组件作为参数并返回一个经过改造的新组件。
+## 为什么使用HOC？(高阶组件不是必须的)
+### 1.抽取重复代码，实现组件复用；
+### 2.条件渲染，控制组件的渲染逻辑(渲染劫持)
+### 3.捕获/劫持被处理组件的生命周期。
+## 命名规范：withXXX()
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- app/my-app/src/App.tsx
+++ app/my-app/src/App.tsx
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import logo from './assets/images/logo.svg';
+import robots from "./mockdata/robots.json";
+import Robot from './components/Robot';
+import styles from './App.module.css';
+import ShoppingCart from './components/ShoppingCart';
+import { ProgressPlugin } from 'webpack';
+import {Spin} from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
+import RobotDiscount from './components/RobotDiscount';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const antIcon = <LoadingOutlined style={{ fontSize: 48 }} spin />;
+
+interface Props{}
+
+interface State {
+  robotGallery: any[];
+  count: number;
 }
 
+const App:React.FC = (props) => {
+  const [count, setCount] = useState<number>(0);
+  const [robotGallery, setRobotGallery] = useState<any>([])
+  const [isLoading, setLoading] = useState(false)
+  const [error, setError] = useState();
+  useEffect(() => {
+    document.title = `点击${count}次`
+  }, [count])
+
+  useEffect(()=>{
+    const fetchData = async () => {
+      setLoading(true)
+      try{
+        const response = await fetch(
+          'https://jsonplaceholder.typicode.com/users'
+        );
+        const data = await response.json();
+        setRobotGallery(data)
+      } catch(e) {
+        setError(e.message)
+      }
+      
+      setLoading(false)
+    }
+    fetchData();
+
+  },[])
+
+  return (
+    <div>
+    <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo"/>
+        <h1>罗伯特机器人购物平台的名字要长</h1>
+      </div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}>
+          Click
+      </button>
+      <span>count: {count}</span>
+      <ShoppingCart />
+      {!error || error!=='' && <div>网站出错：{error}</div>}
+      { !isLoading ?
+        <div className={styles.robotList}>
+        {robotGallery.map((r, index:number) => 
+        ( index % 2 !==0 ?
+        <Robot key={index} id={r.id} name={r.name} email={r.email} />
+        :<RobotDiscount key={index} id={r.id} name={r.name} email={r.email} />
+        )
+        )
+        }
+        </div>
+        : <Spin indicator={antIcon} />
+      }
+    </div>
+    </div>
+  )
+}
 export default App;
