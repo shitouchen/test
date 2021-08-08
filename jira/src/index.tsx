90dd0473947486bc6f89cee840d95dba802ac286
shitouchen <cl18810078360@163.com>
2021-08-08 11:22:07 +0800

    安装devtool

--- jira/src/index.tsx
+++ jira/src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {loadDevTools,} from 'jira-dev-tool';
 
-ReactDOM.render(
+loadDevTools(() => ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
+)
 
 reportWebVitals();
